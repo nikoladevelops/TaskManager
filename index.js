73b2d862ef6930d023ccb7d0e1e50f2b96527818
Nikola <nikoladevelops@gmail.com>
2023-04-01 16:33:11 +0300

    Working on adding User accounts. Each user will have its own tasks.

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
 const tasks = require("./routes/tasks");
 const connectDb = require("./db/connect");
-require("dotenv").config();
 const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// needed for Login/Register functionality
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/User');
 
 // middleware
 app.use(express.static("./public"));
 app.use(express.json());
+
+// session set up
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:true,
+    store: MongoStore.create({mongoUrl:process.env.MONGO_URI}),
+    cookie:{
+        maxAge:1000*60*60*24*7,
+        httpOnly:true
+    }
+}));
+
+// passport set up
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // routes
 app.use("/api/v1/tasks",tasks);
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const router = express.Router();
+
+// TODO
+router.route('/login')
+.get()
+.post();
+
+router.route('/register')
+.get()
+.post();
